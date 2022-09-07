8809800d8b9af8a3728b14e9db0ea23d0b11efaa
Haethor <aym.mougeot@gmail.com>
2022-09-08 01:39:05 +0200

    Aymeric_enemy

---
+++ Assets/Aymeric/Script/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+    public SpriteRenderer graphics;
+    public Animator animator; 
+    
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // Si l'ennemi est quasiment arrivé à sa destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+
+        animator.SetInteger("animationNumber", GameMaster.instance.animation_nb);
+    }
+}
