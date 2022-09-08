93e98e651bc295d1da6fd1c61d59bab97837f695
Haethor <aym.mougeot@gmail.com>
2022-09-08 14:51:27 +0200

    Aymeric_modifTime

--- Assets/Aymeric/Script/LifeManager.cs
+++ Assets/Aymeric/Script/LifeManager.cs
@@ -64,6 +64,7 @@ public class LifeManager : MonoBehaviour
         {
             flashActive = true;
             flashCounter = flashLenght;
+            GameMaster.instance.countdown -= timeHit;
         }
     }
 }
