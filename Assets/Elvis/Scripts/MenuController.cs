8b02fd00a393284d5c3c89f3121fdf5ed2dc273a
Elvis Henry <elvishenry2402@gmail.com>
2022-09-08 18:31:25 +0200

    Elvis - Menu selection

--- Assets/Elvis/Scripts/MenuController.cs
+++ Assets/Elvis/Scripts/MenuController.cs
@@ -6,23 +6,36 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField]
     private GameObject cursor;
-
+    
     private Vector2[] _positionCursor =
     {
         new Vector2(-40, -45),
         new Vector2(3, -45),
         new Vector2(45, -45)
     };
+
+    private int index = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        cursor.transform.localPosition = _positionCursor[2];
+        cursor.transform.localPosition = _positionCursor[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.GetKeyDown(KeyCode.B))
+            ChangeSelection();*/
+    }
+
+    private void ChangeSelection()
+    {
+        index++;
         
+        if (index > _positionCursor.Length -1)
+            index = 0;
+
+        cursor.transform.localPosition = _positionCursor[index];
     }
 }
