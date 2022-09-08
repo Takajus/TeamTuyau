d4fef3e4caa632b4ba97ab4db66b51975f5d4077
Haethor <aym.mougeot@gmail.com>
2022-09-08 14:40:17 +0200

    Aymeric_timer

---
+++ Assets/Aymeric/Script/Timemanager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timemanager : MonoBehaviour
+{
+    private TextMeshProUGUI _txtTimer;
+    [SerializeField]
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _txtTimer = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    GameMaster.instance.countdown -= Time.deltaTime;
+
+    _txtTimer.text = "Timer: " + GameMaster.instance.countdown.ToString();
+    }
+}
